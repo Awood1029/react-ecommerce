c9067464340c2305bb3f204e4f8e998746e3cd15
Arthur Wood <cwarthur1029@gmail.com>
2023-01-09 10:35:11 -0500

    Updating code to current

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,13 +1,13 @@
 import { createContext, useState } from "react";
 
-// Actual value you want to access
 export const UserContext = createContext({
-	currentUser: null,
-	setCurrentUser: () => null,
+  setCurrentUser: () => null,
+  currentUser: null,
 });
 
 export const UserProvider = ({ children }) => {
-	const [currentUser, setCurrentUser] = useState(null);
-	const value = { currentUser, setCurrentUser };
-	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser, setCurrentUser };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,33 +1,47 @@
-import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
-import { UserContext } from "../../contexts/user.context";
-import "./navigation.styles.scss";
+import { Fragment, useContext } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from '../../contexts/user.context';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
-	const { currentUser } = useContext(UserContext);
-	console.log(currentUser);
-	return (
-		<Fragment>
-			<div className="navigation">
-				<Link className="logo-container" to="/">
-					<CrwnLogo className="logo" />
-				</Link>
-				<div className="links-container">
-					<Link className="link" to="/shop">
-						SHOP
-					</Link>
-				</div>
-				<div className="links-container">
-					<Link className="link" to="/sign-in">
-						SIGN IN
-					</Link>
-				</div>
-			</div>
-			<Outlet />
-		</Fragment>
-	);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
+  const signOutHandler = async () => {
+    await signOutUser();
+    setCurrentUser(null);
+  };
+
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutHandler}>
+              {' '}
+              SIGN OUT{' '}
+            </span>
+          ) : (
+            <Link className='nav-link' to='/auth'>
+              SIGN IN
+            </Link>
+          )}
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
 };
 
 export default Navigation;
